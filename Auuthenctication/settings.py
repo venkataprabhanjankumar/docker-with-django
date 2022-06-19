1296a33844129ee4e592c92a834af74903c21e5f
venkataprabhanjankumar <venkataprabhanjankumar@gmail.com>
2022-06-19 12:51:37 +0530

    commit

--- Auuthenctication/settings.py
+++ Auuthenctication/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-g%@)6ly-i35k35xah$j#bx535gt4xkz%)dk)g(9d)+2lo*co6u'
+SECRET_KEY = 'secreat'
 
 DEBUG = True
 
